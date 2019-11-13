2b8030e81d4a7b86fdc16058a488e53083359953
jrpineda <53352598+jrpineda@users.noreply.github.com>
2019-11-13 17:44:27 -0500

    Update Repdata 2.R

--- Repdata 2.R
+++ Repdata 2.R
@@ -1,10 +1,73 @@
+library(downloader)
+library(tidyverse)
+library(ggrepel)
+library(ggthemes)
+
 url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2" 
 download(url, dest="repdata_data_StormData.csv", mode="wb")
 storm <- read.csv("repdata_data_StormData.csv")
 
-#First we look at what the most common event is in the data
+#The Event type category is a bit messy, so I make some adjustments
+#to the largest categories to clean up based on the supp docs
 
-count <- arrange(storm %>% count(EVTYPE), desc(n))
+storm$EVTYPE[storm$EVTYPE %in% "TSTM WIND"] <- "THUNDERSTORM WIND"
+storm$EVTYPE[storm$EVTYPE %in% "THUNDERSTORM WINDS"] <- "THUNDERSTORM WIND"
+storm$EVTYPE[storm$EVTYPE %in% "MARINE TSTM WIND"] <- "MARINE THUNDERSTORM WIND"
+
+
+#First we look at what the top 20 most common event is in the data
+
+count <- head(arrange(storm %>% count(EVTYPE), desc(n)), n=20)
+count$rank <- seq(20)
+
+
+#Next we see which caused the most property damage
+
+propdmg <- head(arrange(storm %>% 
+            group_by(EVTYPE) %>% 
+            summarize(PROPDMG = sum(PROPDMG)),
+            desc(PROPDMG)), n = 10)
+
+propdmg <- arrange(merge(propdmg, count))
+
+#Next a graph of cost per incident
+
+ggplot(propdmg, aes(x = PROPDMG, y = n))+
+    geom_point()+
+    geom_label_repel(size = 2.5, label=propdmg$EVTYPE)+
+    labs(x = "Property Damage",
+         y = "Number of Events",
+         title = "Damage per Event (TOP 10*)",
+         caption = "*In terms of damage in dollars")+
+    theme_economist()
+
+
+################################################
+
+#INJURIES AND FATALITIES
+
+inj <- head(arrange(storm %>% 
+              group_by(EVTYPE) %>% 
+              summarize(INJURIES = sum(INJURIES)),
+              desc(INJURIES)), n = 20)
+
+ftl <- head(arrange(storm %>% 
+                      group_by(EVTYPE) %>% 
+                      summarize(FATALITIES = sum(FATALITIES)),
+                    desc(FATALITIES)), n = 20)
+
+hurt <- head(merge(inj, ftl), n = 10)
+hurt$totals <- (hurt$INJURIES+hurt$FATALITIES)
+
+hurt2 <- gather(hurt, key = Incident, value = totals, -EVTYPE, -totals)
+
+ggplot(hurt2, aes(EVTYPE, totals))+
+    geom_bar(aes(fill = Incident), position = position_stack(reverse = TRUE),
+          stat="identity")+coord_flip()+
+          labs(y = "Total Individuals",
+               x = NULL,
+               title = "Fatalities and Injuries by Type of Event")+
+          theme_igray()
 
 
 
